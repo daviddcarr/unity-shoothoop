36a876ed8a3f358ebc3768bb28d15e945327d878
David Carr <david@yellow7.com>
2016-06-23 10:03:04 -0500

    Initialize repository for ShootHoop  Unity Game

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawner : MonoBehaviour {
+
+	public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float speed = 3.0f;
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			Rigidbody rb = InstantiateBall ();
+			rb.velocity = Vector3.left * speed;
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			Rigidbody rb = InstantiateBall ();
+			rb.velocity = Vector3.right * speed;
+		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			Rigidbody rb = InstantiateBall ();
+			rb.velocity = Vector3.forward * speed;
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			Rigidbody rb = InstantiateBall ();
+			rb.velocity = Vector3.back * speed;
+		}
+
+	}
+
+	Rigidbody InstantiateBall () {
+		GameObject instance = Instantiate (ballPrefab);
+		Rigidbody rb = instance.GetComponent<Rigidbody> ();
+		return rb;
+	}
+}
---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotationSpeed = 3.0f;
+		float mouseY = Input.GetAxis ("Mouse Y") * rotationSpeed;
+		float mouseX = Input.GetAxis ("Mouse X") * rotationSpeed;
+
+		//Rotate player object (parent)
+		transform.localRotation = transform.localRotation * Quaternion.Euler (0, mouseX, 0);
+
+		//Grab the camera object and rotate it (Child)
+		Camera camera = GetComponentInChildren<Camera> ();
+		camera.transform.localRotation = camera.transform.localRotation * Quaternion.Euler (-mouseY, 0, 0);
+		//transform.localRotation = transform.localRotation * Quaternion.Euler (0, 1, 0);
+	}
+}
