2ee26bf5f3d811c99d0efce1be68469d0f7b0b60
David Carr <david@yellow7.com>
2016-06-23 16:33:39 -0500

    Update game to include menu scene.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			LoadNextScene ();
+		}
+	}
+	void LoadNextScene() {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+}
