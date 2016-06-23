a5a3ed2e62057d8f15709bec9d76d5e739206ddd
David Carr <david@yellow7.com>
2016-06-23 12:34:44 -0500

    Update to Section 3, Lesson 32 content. Basketballs now launch from player camera using the left mouse button.

---
+++ Assets/Scripts/BallLauncher.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallLauncher : MonoBehaviour {
+
+	//Create field to input prefab model in inspector
+	public GameObject ballPrefab;
+	public float ballSpeed = 15.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			InstantiateBall (ballSpeed);
+		}
+
+	}
+
+	void InstantiateBall (float speed) {
+		GameObject instance = Instantiate (ballPrefab);
+		instance.transform.position = transform.position;
+		Rigidbody rb = instance.GetComponent<Rigidbody> ();
+		Camera camera = GetComponentInChildren<Camera>();
+		rb.velocity = (camera.transform.rotation * Vector3.forward) * speed;
+	}
+}
